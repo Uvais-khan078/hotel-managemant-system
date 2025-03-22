e9dca50b6cf640b0bdc4e9d3c907ba64836a839e
Mohd uvais wali khan <uvaiswalikhan@gmail.com>
2025-03-22 09:37:35 +0530

    Initial commit

---
+++ src/components/Dinein.jsx
@@ -0,0 +1,68 @@
+export default function Dinein() {
+    return (
+        <>
+  <body>
+  <section class="text-gray-600 body-font">
+      <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-col">
+          <div class="h-1 bg-gray-200 rounded overflow-hidden">
+            <div class="w-24 h-full bg-indigo-500"></div>
+          </div>
+          <div class="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
+            <h1 class="sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0">Zaika Restraunt</h1>
+            <p class="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0">"welcome to zaika we hope you enjoy your time with us.if there is anything you need,please don't hesitate to ask."</p>
+          </div>
+        </div>
+        <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+          <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
+          <a>
+          <div class="rounded-lg h-64 overflow-hidden">
+              <img alt="content" class="object-cover object-center h-full w-full" src="https://lestresorsderable.com/img/cms/canadian-breakfast-260x180.jpeg"/>
+            </div>
+            <h2 class="text-xl font-medium title-font text-gray-900 mt-5">Breakfast</h2>
+            <p class="text-base leading-relaxed mt-2">6:30am-10:30am</p>
+            
+              
+            </a>
+          </div>
+          <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
+          <a>
+          <div class="rounded-lg h-64 overflow-hidden">
+              <img alt="content" class="object-cover object-center h-full w-full" src="https://th.bing.com/th/id/R.2803fb0e0affa13e779713911aaf21dc?rik=sw5Q1%2beOqg3g3A&riu=http%3a%2f%2fstatic1.squarespace.com%2fstatic%2f6217a6968b376a434418ae8b%2ft%2f6416043616fc5e2dca82c202%2f1679164471002%2fShow%2bYour%2bLove%2bwith%2bour%2bMiso%2bTasty%2bBento%2bBox%2bLunch%2bIdeas.jpg%3fformat%3d1500w&ehk=PocYC6RSzaCqcjgF3%2bk3qujjC1omsMastEE2nJIlVpY%3d&risl=&pid=ImgRaw&r=0"/>
+            </div>
+            <h2 class="text-xl font-medium title-font text-gray-900 mt-5">lunch</h2>
+            <p class="text-base leading-relaxed mt-2">12:00pm-2:00pm</p>
+            
+             
+            </a>
+          </div>
+          <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
+          <a>
+            <div class="rounded-lg h-64 overflow-hidden">
+              <img alt="content" class="object-cover object-center h-full w-full" src="https://png.pngtree.com/background/20230606/original/pngtree-an-elegant-dinner-table-set-in-the-dark-is-shown-picture-image_2879798.jpg"/>
+            </div>
+            <h2 class="text-xl font-medium title-font text-gray-900 mt-5">Dinner</h2>
+            <p class="text-base leading-relaxed mt-2">8:00pm-10:00pm</p>
+            
+           
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="flex" >
+       <div class="flex flex-col m-auto " >
+          <h1 class="text-4xl font-bold "> Customer Details</h1>
+          <h2>order no :- 23002</h2>
+          <input type="date" name="date" id="date"/>
+          <h2>expected waiting perior :- 10 min.</h2>
+          </div>
+          </div>
+      
+   
+     </section>
+ 
+</body>
+</>
+
+    )
+}
